eb5643c6a5ca0915603122ed97d365fcb87fde5d
Ruslan Kabalin <ruslan.kabalin@gmail.com>
2025-06-07 18:51:29 +0100

    Fix external_api call and bump moodle requirements to 4.2

--- classes/external/get_token.php
+++ classes/external/get_token.php
@@ -19,10 +19,10 @@ namespace local_jwttomoodletoken\external;
 require_once($CFG->dirroot . '/local/jwttomoodletoken/phpjwt/JWT.php');
 require_once($CFG->dirroot . '/local/jwttomoodletoken/phpjwt/Key.php');
 
-use external_api;
-use external_function_parameters;
-use external_single_structure;
-use external_value;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_single_structure;
+use core_external\external_value;
 use context_system;
 use moodle_exception;
 use local_jwttomoodletoken\local\helper;
--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022091200;
-$plugin->requires = 2019052000; // Moodle 3.9
+$plugin->version = 2025060700;
+$plugin->requires = 2023042400; // Moodle 4.2
 $plugin->component = 'local_jwttomoodletoken';
 
